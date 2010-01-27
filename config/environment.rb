a57378f65e27a84ec08f3efe583d65bef17febfa
Łukasz Piestrzeniewicz <bragi@ragnarson.com>
2010-01-27 12:26:43 +0100

    Cleaned up to get test running (not passing of course ;) )

--- config/environment.rb
+++ config/environment.rb
@@ -24,12 +24,10 @@ Rails::Initializer.run do |config|
   # They can then be installed with "rake gems:install" on new installations.
   # You have to specify the :lib option for libraries, where the Gem name (sqlite3-ruby) differs from the file itself (sqlite3)
   config.gem "haml"
-  config.gem(
-    'mislav-will_paginate',
-    :version => '~> 2.2.3',
-    :lib => 'will_paginate', 
-    :source => 'http://gems.github.com'
-  )
+  config.gem 'mislav-will_paginate', :version => '=2.2.3', :lib => 'will_paginate'
+  config.gem "pdf-writer", :version => "=1.1.2", :lib => "pdf/writer"
+  config.gem "faker", :version => "=0.3.1"
+  config.gem "BlueCloth", :version => "=1.0.1", :lib => "bluecloth"
 
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
