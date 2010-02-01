431d42f182aba97d243276b1e5feb266be9a3ae6
Fernando Guillen <fguillen.mail@gmail.com>
2010-02-01 11:37:06 +0100

    preparing to euruko 2010
    Signed-off-by: Fernando Guillen <fguillen.mail@gmail.com>

--- config/environment.rb
+++ config/environment.rb
@@ -87,3 +87,5 @@ ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
   :cool => "%A %d %B %Y at %H:%M %p",
   :date => "%A %d %B %Y"
 )
+
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,3 +22,6 @@ config.action_controller.perform_caching             = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
+
+# Mail method
+config.action_mailer.delivery_method = :sendmail
